6e55e6af3a7461113786a610d2feb7e9402bcb25
Yoo Seungmin <dnjfht@naver.com>
2023-01-05 22:21:18 +0900

    컴포넌트 생성

---
+++ .history/App_20230105221900.js
@@ -0,0 +1,7 @@
+import { StatusBar } from "expo-status-bar"
+import {} from "react-native"
+import styled from "@emotion/native"
+
+export default function App() {
+    return <StatusBar style="auto" />
+}
