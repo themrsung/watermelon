e224a1117afc4a0469a382f145ad9f80a25f3142
MJ Sung <themrsung@gmail.com>
2023-01-06 08:58:55 +0900

    unhappy irst refactoring

---
+++ api/apiSettings.js
@@ -0,0 +1,5 @@
+export const SERVER_URL = "https://watermelon-server-b3.herokuapp.com"
+
+export const SERVER_GET_SUCCESS = "성공적으로 데이터를 불러왔습니다."
+export const SERVER_GET_FAILURE_RESPONSE_NOT_OK =
+    "불러온 데이터가 유효하지 않습니다."
--- api/musicApi.js
+++ api/musicApi.js
@@ -1,7 +1,7 @@
 // Music API
 
 import axios from "axios"
-import { SERVER_URL } from "./serverUrl"
+import { SERVER_URL } from "./apiSettings"
 import uuid from "react-native-uuid"
 
 // 글쓰기입니다. 오류내시면 서버에 그대로 들어가요.
@@ -16,17 +16,22 @@ export const createMusic = (music) => {
 // 글 1개를 가져옵니다.
 export const getMusic = async (uuid) => {
     const response = await axios.get(SERVER_URL + "/musics/" + uuid)
-    if (response) {
-        return response.data
+
+    if (!response) {
+        return {}
     }
+
+    return response.data
 }
 
 // 글을 전부 가져옵니다. filter는 알아서 돌리세요ㅋ
 export const getMusics = async () => {
     const response = await axios.get(SERVER_URL + "/musics")
-    if (response) {
-        return response.data
+    if (!response) {
+        return []
     }
+
+    return response.data
 }
 
 // uuid에 해당하는 글을 두번째 파라미터의 글로 수정합니다.
