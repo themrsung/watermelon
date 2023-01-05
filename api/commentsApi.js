50177863d88e7f808f186ee38c172a2af9f2f841
MJ Sung <themrsung@gmail.com>
2023-01-05 21:23:01 +0900

    export!

--- api/commentsApi.js
+++ api/commentsApi.js
@@ -1,28 +1,28 @@
 // Comments API
 
 // musicUuid에 해당하는 글에 댓글을 작성합니다.
-const writeComment = (musicUuid, comment) => {
+export const writeComment = (musicUuid, comment) => {
     return "uuid"
 }
 
 // musicUuid에 해당하는 글에 댓글을 작성합니다.
-const getComment = (commentUuid) => {
+export const getComment = (commentUuid) => {
     return {
         uuid: commentUuid
     }
 }
 
 // musicUuid에 해당하는 글의 댓글을 전부 가져옵니다. filter는 알아서 돌리세요.
-const getComments = (musicUuid) => {
+export const getComments = (musicUuid) => {
     return []
 }
 
 // commentUuid에 해당하는 댓글을 두번째 파라미터의 댓글로 수정합니다.
-const editComment = (commentUuid, newComment) => {
+export const editComment = (commentUuid, newComment) => {
     return "성공"
 }
 
 // commentUuid에 해당하는 댓글을 삭제합니다.
-const deleteComment = (commentUuid) => {
+export const deleteComment = (commentUuid) => {
     return "성공"
 }
--- api/playlistsApi.js
+++ api/playlistsApi.js
@@ -1,12 +1,12 @@
 // Playlists API
 
 // playlist를 등록합니다.
-const createPlaylist = (playlist) => {
+export const createPlaylist = (playlist) => {
     return "uuid"
 }
 
 // playlistUuid에 해당하는 playlist를 가져옵니다.
-const getPlaylist = (playlistUuid) => {
+export const getPlaylist = (playlistUuid) => {
     return {
         uuid: playlistUuid,
         content: []
@@ -14,16 +14,16 @@ const getPlaylist = (playlistUuid) => {
 }
 
 // playlist를 전부 가져옵니다.
-const getPlaylists = () => {
+export const getPlaylists = () => {
     return []
 }
 
 // playlistUuid에 해당하는 playlist를 newPlaylist로 수정합니다.
-const editPlaylist = (playlistUuid, newPlaylist) => {
+export const editPlaylist = (playlistUuid, newPlaylist) => {
     return "성공"
 }
 
 // playlistUuid에 해당하는 playlist를 삭제합니다.
-const deletePlaylist = (playlistUuid) => {
+export const deletePlaylist = (playlistUuid) => {
     return "성공"
 }
